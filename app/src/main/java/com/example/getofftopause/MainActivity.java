e06f5510669316f72ecb4b9b5838d6aa042fbb03
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2021-05-23 05:59:30 +0900

    位置情報と身体活動の両方が無効の場合、「制御を開始」ボタンを無効化する

--- app/src/main/java/com/example/getofftopause/MainActivity.java
+++ app/src/main/java/com/example/getofftopause/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.View;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
@@ -15,12 +16,26 @@ import androidx.core.app.ActivityCompat;
 import androidx.preference.EditTextPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 import androidx.preference.SwitchPreference;
 
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 
-public class MainActivity extends AppCompatActivity {
-    private boolean enabled = false;
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener, View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+
+    private boolean enabled;
+
+    private ExtendedFloatingActionButton button;
+    private SharedPreferences sharedPreferences;
+    private Intent intent;
+
+    private void updateButtonEnabled() {
+        button.setEnabled(
+                sharedPreferences.getBoolean(getString(R.string.location_key), true) ||
+                        sharedPreferences.getBoolean(getString(R.string.activity_recognition_key), true));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,29 +49,43 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }
 
-        ExtendedFloatingActionButton button = findViewById(R.id.floatingActionButton);
+        button = findViewById(R.id.floatingActionButton);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        intent = new Intent(getApplication(), MediaControlService.class);
 
-        Intent intent = new Intent(getApplication(), MediaControlService.class);
+        button.setOnClickListener(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
 
-        button.setOnClickListener(v -> {
-            if (enabled) {
-                enabled = false;
-                button.setIcon(AppCompatResources.getDrawable(this, R.drawable.ic_baseline_music_note_24));
-                button.setText(R.string.enable_media_control);
+        enabled = false;
+        updateButtonEnabled();
+    }
 
-                stopService(intent);
-            } else {
-                enabled = true;
-                button.setIcon(AppCompatResources.getDrawable(this, R.drawable.ic_baseline_stop_24));
-                button.setText(R.string.disable_media_control);
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(getString(R.string.location_key)) || key.equals(getString(R.string.activity_recognition_key))) {
+            updateButtonEnabled();
+        }
+    }
 
-                startForegroundService(intent);
-            }
-        });
+    @Override
+    public void onClick(View v) {
+        if (enabled) {
+            enabled = false;
+            button.setIcon(AppCompatResources.getDrawable(this, R.drawable.ic_baseline_music_note_24));
+            button.setText(R.string.enable_media_control);
 
+            stopService(intent);
+        } else {
+            enabled = true;
+            button.setIcon(AppCompatResources.getDrawable(this, R.drawable.ic_baseline_stop_24));
+            button.setText(R.string.disable_media_control);
+
+            startForegroundService(intent);
+        }
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+        private SharedPreferences sharedPreferences;
         private SwitchPreference locationPreference;
         private final ActivityResultLauncher<String[]> requestLocationPermissionsLauncher =
                 registerForActivityResult(new ActivityResultContracts.RequestMultiplePermissions(), result -> {
@@ -71,10 +100,9 @@ public class MainActivity extends AppCompatActivity {
                         activityRecognitionPreference.setChecked(true);
                     }
                 });
-        private SharedPreferences sharedPreferences;
 
         private void updateTitleAndSummary() {
-            if (sharedPreferences.getBoolean(getString(R.string.location_key), false)) {
+            if (sharedPreferences.getBoolean(getString(R.string.location_key), true)) {
                 activityRecognitionPreference.setTitle(R.string.activity_recognition_title_with_location);
                 activityRecognitionPreference.setSummary(R.string.activity_recognition_summary_with_location);
             } else {
@@ -121,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 });
 
-                getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+                sharedPreferences.registerOnSharedPreferenceChangeListener(this);
             }
 
             if (activityRecognitionPreference != null) {
