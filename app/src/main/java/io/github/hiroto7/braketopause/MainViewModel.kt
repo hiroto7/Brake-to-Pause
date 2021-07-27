ae5ee7750cb0227cc962628a5f488769f5d5c05e
hiroto7 <32275337+hiroto7@users.noreply.github.com>
2021-07-27 23:44:10 +0900

    コードを整理

--- app/src/main/java/io/github/hiroto7/braketopause/MainViewModel.kt
+++ app/src/main/java/io/github/hiroto7/braketopause/MainViewModel.kt
@@ -12,46 +12,16 @@ class MainViewModel(application: Application) : AndroidViewModel(application),
         SharedPreferences.OnSharedPreferenceChangeListener {
     private val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(application)
 
-    val speedThreshold = MutableLiveData(
-            sharedPreferences.getInt(
-                    application.getString(R.string.speed_threshold_key),
-                    8
-            )
-    )
+    val speedThreshold = MutableLiveData(sharedPreferences.getInt(application.getString(R.string.speed_threshold_key), 8))
 
-    private val isInVehicleSelected = MutableLiveData(
-            sharedPreferences.getBoolean(
-                    application.getString(R.string.in_vehicle_key),
-                    true
-            )
-    )
-    private val isOnBicycleSelected = MutableLiveData(
-            sharedPreferences.getBoolean(
-                    application.getString(R.string.on_bicycle_key),
-                    true
-            )
-    )
-    private val isRunningSelected = MutableLiveData(
-            sharedPreferences.getBoolean(
-                    application.getString(R.string.running_key),
-                    true
-            )
-    )
-    private val isWalkingSelected = MutableLiveData(
-            sharedPreferences.getBoolean(
-                    application.getString(R.string.walking_key),
-                    true
-            )
-    )
+    private val isInVehicleSelected = MutableLiveData(sharedPreferences.getBoolean(application.getString(R.string.in_vehicle_key), true))
+    private val isOnBicycleSelected = MutableLiveData(sharedPreferences.getBoolean(application.getString(R.string.on_bicycle_key), true))
+    private val isRunningSelected = MutableLiveData(sharedPreferences.getBoolean(application.getString(R.string.running_key), true))
+    private val isWalkingSelected = MutableLiveData(sharedPreferences.getBoolean(application.getString(R.string.walking_key), true))
 
     val selectedActivities = MediatorLiveData<Set<Int>>()
 
-    val usesActivityRecognition = MutableLiveData(
-            sharedPreferences.getBoolean(
-                    application.getString(R.string.activity_recognition_key),
-                    true
-            )
-    )
+    val usesActivityRecognition = MutableLiveData(sharedPreferences.getBoolean(application.getString(R.string.activity_recognition_key), true))
 
 
     init {
@@ -75,43 +45,14 @@ class MainViewModel(application: Application) : AndroidViewModel(application),
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
-        if (key == getApplication<Application>().getString(R.string.speed_threshold_key)) {
-            speedThreshold.value = sharedPreferences.getInt(
-                    getApplication<Application>().getString(R.string.speed_threshold_key),
-                    8
-            )
-        }
+        when (key) {
+            getApplication<Application>().getString(R.string.speed_threshold_key) -> speedThreshold.value = sharedPreferences.getInt(getApplication<Application>().getString(R.string.speed_threshold_key), 8)
+            getApplication<Application>().getString(R.string.activity_recognition_key) -> usesActivityRecognition.value = sharedPreferences.getBoolean(getApplication<Application>().getString(R.string.activity_recognition_key), true)
 
-        if (key == getApplication<Application>().getString(R.string.activity_recognition_key)) {
-            usesActivityRecognition.value = sharedPreferences.getBoolean(
-                    getApplication<Application>().getString(R.string.activity_recognition_key),
-                    true
-            )
-        }
-
-        if (key == getApplication<Application>().getString(R.string.in_vehicle_key)) {
-            isInVehicleSelected.value = sharedPreferences.getBoolean(
-                    getApplication<Application>().getString(R.string.in_vehicle_key),
-                    true
-            )
-        }
-        if (key == getApplication<Application>().getString(R.string.on_bicycle_key)) {
-            isOnBicycleSelected.value = sharedPreferences.getBoolean(
-                    getApplication<Application>().getString(R.string.on_bicycle_key),
-                    true
-            )
-        }
-        if (key == getApplication<Application>().getString(R.string.running_key)) {
-            isRunningSelected.value = sharedPreferences.getBoolean(
-                    getApplication<Application>().getString(R.string.running_key),
-                    true
-            )
-        }
-        if (key == getApplication<Application>().getString(R.string.walking_key)) {
-            isWalkingSelected.value = sharedPreferences.getBoolean(
-                    getApplication<Application>().getString(R.string.walking_key),
-                    true
-            )
+            getApplication<Application>().getString(R.string.in_vehicle_key) -> isInVehicleSelected.value = sharedPreferences.getBoolean(getApplication<Application>().getString(R.string.in_vehicle_key), true)
+            getApplication<Application>().getString(R.string.on_bicycle_key) -> isOnBicycleSelected.value = sharedPreferences.getBoolean(getApplication<Application>().getString(R.string.on_bicycle_key), true)
+            getApplication<Application>().getString(R.string.running_key) -> isRunningSelected.value = sharedPreferences.getBoolean(getApplication<Application>().getString(R.string.running_key), true)
+            getApplication<Application>().getString(R.string.walking_key) -> isWalkingSelected.value = sharedPreferences.getBoolean(getApplication<Application>().getString(R.string.walking_key), true)
         }
     }
 }
